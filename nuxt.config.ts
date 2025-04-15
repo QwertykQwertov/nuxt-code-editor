1545b211193cab264dec617faf762474fb1754d0
QwertykQwertov <qwertykqwertov@mail.ru>
2025-04-15 15:51:23 +0300

    Разбиение app.vue на верхнеуровневые компоненты

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,41 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-11-01",
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
+  css: ['~/assets/css/main.css'],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   app: {
     head: {
-      title: "Code Editor",
+      title: 'Code Editor',
     },
   },
-});
+
+  modules: ['@nuxtjs/google-fonts', '@nuxt/icon'],
+  googleFonts: {
+    display: 'swap',
+    families: {
+      'Open Sans': {
+        wght: '300..800',
+        ital: '300..800',
+      },
+    },
+  },
+  icon: {
+    mode: 'svg',
+    aliases: {
+      code: 'heroicons:code-bracket-square-solid',
+      puzzle: 'heroicons:puzzle-piece-solid',
+      fire: 'heroicons:fire-solid',
+      cog: 'heroicons:cog-6-tooth-solid',
+      plane: 'heroicons:paper-airplane-solid',
+      bars: 'heroicons:bars-3-solid',
+      xMark: 'heroicons:x-mark-solid',
+    },
+  },
+})
