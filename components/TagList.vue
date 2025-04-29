01e69808e37928322785deb2e279657d5526efad
QwertykQwertov <qwertykqwertov@mail.ru>
2025-04-29 15:14:54 +0300

    Добавлена страница с плейлистами и разбита на компоненты

---
+++ components/TagList.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const tags = [
+  'Productivity',
+  'Development',
+  'Design',
+  'Utility',
+  'Integration',
+  'Testing',
+  'Debugging',
+  'Formatting',
+  'Collaboration',
+]
+</script>
+
+<template>
+  <ul class="flex flex-col gap-y-1">
+    <TagListItem v-for="tag in tags" :key="tag">{{ tag }} </TagListItem>
+  </ul>
+</template>
