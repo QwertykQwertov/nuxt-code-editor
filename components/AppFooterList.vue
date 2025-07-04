5b2adb243fbd0e59ca7a127c69b1c2686a2ebeac
QwertykQwertov <QwertykQwertov@mail.ru>
2025-07-04 21:03:58 +0300

    Изменен компонент с логотипом и изменены ссылки

--- components/AppFooterList.vue
+++ components/AppFooterList.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-defineProps<{ list: { heading: string; items: string[] } }>()
+interface Item {
+  label: 'string'
+  link: 'string'
+}
+
+defineProps<{ list: { heading: string; items: Item[] } }>()
 </script>
 
 <template>
   <div class="flex flex-col gap-y-3 sm:gap-y-4">
     <AppFooterListHeading>{{ list.heading }}</AppFooterListHeading>
     <ul class="flex flex-col gap-y-1 sm:gap-y-2">
-      <AppFooterListItem v-for="item in list.items" :key="item">
+      <AppFooterListItem v-for="item in list.items" :key="item.label" :link="item.link">
         {{ item }}
       </AppFooterListItem>
     </ul>
